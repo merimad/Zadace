979130ff47728640935643ecad732a9df901bc31
Merima <Merima@merimas-mac.local>
2016-06-24 18:20:45 +0200

    Prvi projekat

---
+++ 2406_1_hELLOwORLD/2406_1_hELLOwORLD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2406_1_hELLOwORLD
+//
+//  Created by Merima on 6/24/16.
+//  Copyright © 2016 Work in progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
