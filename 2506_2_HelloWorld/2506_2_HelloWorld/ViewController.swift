19cb5f3b3585469ca7d3816a6d7247d79319f4e2
Merima <Merima@merimas-mac.local>
2016-06-25 15:36:14 +0200

    proba

---
+++ 2506_2_HelloWorld/2506_2_HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2506_2_HelloWorld
+//
+//  Created by Merima on 6/25/16.
+//  Copyright © 2016 Work in progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
