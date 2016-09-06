35cf784f7be22b4ce46fc51413a134e15e93adf5
Merima <merima@merimas-mac.local>
2016-09-06 14:08:00 +0200

    Zadatak 7

--- FoodTracker_v1 2/FoodTracker_v1/Meal.swift
+++ FoodTracker_v1 2/FoodTracker_v1/Meal.swift
@@ -5,8 +5,20 @@
 //
 
 import UIKit
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    // MARK: Types
+    struct  PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+        
+    }
     
     var name: String
     var photo: UIImage?
@@ -17,9 +29,23 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+        super.init()
         // Initialization should fail if there is no name or if the rating is negative
         if name.isEmpty || rating < 0 {
             return nil
         }
         
-    }}
+    }
+    // MARK: NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)    }}
